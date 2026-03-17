6c0aade67a6dc34843c6169c3938b3d7907cf683
mikl81 <mikelouisfitz@gmail.com>
2026-03-17 01:21:43 +0000

    keygen implemented

--- keygen.c
+++ keygen.c
@@ -6,8 +6,51 @@ rand()Links to an external site. is just fine. The last character keygen outputs
 Any error text must be output to stderr
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+char intToChar(int val){
+    int base = 65; //Capital A
+    int modo = val%27; //Limit to accepted chars
+    int ascii; 
+    if(modo == 26){ //26 will represent space
+        ascii = 32;
+    } else {
+        ascii = base + modo; //Otherwise we get the character ascii value
+    };
 
-void main(){
-    printf("Test");
+    return (char)ascii; //Cast on return
+};
+
+void main(int argc, char **argv){
+    //Check valid number of arguments
+    if(argc != 2){ 
+        fprintf(stderr, "Invalid number of arguments.\n");
+        exit(1);
+    };
+    //Parse input
+    int numChars = atoi(argv[1]);
+    if(!numChars || numChars < 1){
+        fprintf(stderr, "Invalid number of characters requested.\n");
+        exit(1);
+    };
+
+    //Var declarations
+    int i;
+    char keyStr[numChars+1];
+
+    //Init rand
+    srand(time(NULL));
+
+    for(i=0; i < numChars; i++){
+        int rand_num = rand() % 100;
+        char letter = intToChar(rand_num);
+        keyStr[i] = letter;
+    };
+
+    keyStr[numChars+1] = '\0'; //Terminator
+    printf("%s\n", keyStr); //Add newline 
+    exit(0);
+
 };
