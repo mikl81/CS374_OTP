188ed4f598aa50c304f088ee02e83b296e06d245
mikl81 <mikelouisfitz@gmail.com>
2026-03-17 02:09:41 +0000

    Copied over instructs and template code

--- enc_server.c
+++ enc_server.c
@@ -1,7 +1,121 @@
+/*
+This program is the encryption server and will run in the background as a daemon.
 
+Its function is to perform the actual encoding, as described above in the Wikipedia quote.
+This program will listen on a particular port/socket, assigned when it is first ran (see syntax below).
+Upon execution, enc_server must output an error if it cannot be run due to a network error, 
+such as the ports being unavailable. When a connection is made, enc_server must call accept()
+to generate the socket used for actual communication, and then use a separate process to 
+handle the rest of the servicing for this client connection (see below), which will occur 
+on the newly accepted socket.
+
+This child process of enc_server must first check to make sure it is communicating with enc_client
+(see enc_client, below).
+After verifying that the connection to enc_server is coming from enc_client, then this child 
+receives plaintext and a key from enc_client via the connected socket.
+
+The enc_server child will then write back the ciphertext to the enc_client process that it is 
+connected to via the same connected socket. Note that the key passed in must be at least as big
+as the plaintext.
+*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-
-void main(){
-    printf("Test");
+// Error function used for reporting issues
+void error(const char *msg) {
+  perror(msg);
+  exit(1);
 };
+
+char* encoder(const char *toEncode, const char *key){
+    size_t length = strlen(toEncode);
+    
+};
+
+// Set up the address struct for the server socket
+void setupAddressStruct(struct sockaddr_in* address, 
+                        int portNumber){
+ 
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address)); 
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+  // Allow a client at any address to connect to this server
+  address->sin_addr.s_addr = INADDR_ANY;
+}
+
+int main(int argc, char *argv[]){
+  int connectionSocket, charsRead;
+  char buffer[256];
+  struct sockaddr_in serverAddress, clientAddress;
+  socklen_t sizeOfClientInfo = sizeof(clientAddress);
+
+  // Check usage & args
+  if (argc < 2) { 
+    fprintf(stderr,"USAGE: %s port\n", argv[0]); 
+    exit(1);
+  } 
+  
+  // Create the socket that will listen for connections
+  int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenSocket < 0) {
+    error("ERROR opening socket");
+  }
+
+  // Set up the address struct for the server socket
+  setupAddressStruct(&serverAddress, atoi(argv[1]));
+
+  // Associate the socket to the port
+  if (bind(listenSocket, 
+          (struct sockaddr *)&serverAddress, 
+          sizeof(serverAddress)) < 0){
+    error("ERROR on binding");
+  }
+
+  // Start listening for connetions. Allow up to 5 connections to queue up
+  listen(listenSocket, 5); 
+  
+  // Accept a connection, blocking if one is not available until one connects
+  while(1){
+    // Accept the connection request which creates a connection socket
+    connectionSocket = accept(listenSocket, 
+                (struct sockaddr *)&clientAddress, 
+                &sizeOfClientInfo); 
+    if (connectionSocket < 0){
+      error("ERROR on accept");
+    }
+
+    printf("SERVER: Connected to client running at host %d port %d\n", 
+                          ntohs(clientAddress.sin_addr.s_addr),
+                          ntohs(clientAddress.sin_port));
+
+    // Get the message from the client and display it
+    memset(buffer, '\0', 256);
+    // Read the client's message from the socket
+    charsRead = recv(connectionSocket, buffer, 255, 0); 
+    if (charsRead < 0){
+      error("ERROR reading from socket");
+    }
+    printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+
+    // Send a Success message back to the client
+    charsRead = send(connectionSocket, 
+                    "I am the server, and I got your message", 39, 0); 
+    if (charsRead < 0){
+      error("ERROR writing to socket");
+    }
+    // Close the connection socket for this client
+    close(connectionSocket); 
+  }
+  // Close the listening socket
+  close(listenSocket); 
+  return 0;
+}
